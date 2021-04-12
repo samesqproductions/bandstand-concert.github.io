9f75dc89d472eddad11103b29d9b21d59a2bc163
DoraTheExplorax <randykethcena@gmail.com>
2021-04-12 18:18:26 +0530

    work

--- countdown.js
+++ countdown.js
@@ -22,7 +22,7 @@
               countdown = document.getElementById("countdown"),
               content = document.getElementById("content");
 
-          headline.innerText = "It's my birthday!";
+          headline.innerText = "Registrations have ended";
           countdown.style.display = "none";
           content.style.display = "block";
 
